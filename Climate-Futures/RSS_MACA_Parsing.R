a5b339a4fd7cf951f4e9b403cf3c49f383e944eb
Amber Childress-Runyon <acrunyon@users.noreply.github.com>
2020-01-02 13:32:59 -0700

    Merged with A.Runyon code
    Parse over whole time period. Save as init RData file and stop after parsing.  CF creation left for PlotTable script.

--- Climate-Futures/RSS_MACA_Parsing.R
+++ Climate-Futures/RSS_MACA_Parsing.R
@@ -16,7 +16,6 @@ library(ncdf4)
 ############################# User-defined initials #################################
 
 ####Location for data extraction
-SiteID = ""
 # Lat and lon coordinates - Cell resolution is 0.041666 degrees, reference coordinates are cell centers
 Lat = 32.196
 Lon = -110.525
@@ -25,15 +24,16 @@ SiteID
 
 ###Parameters for extracting data from NetCDF files
 #Specify top-level directory where MACA data is stored (all scenarios and variables)
-#MACADir = "F:/ClimateData/MACAv2Metdata" #no '/' at the end
-MACADir = "C:/Users/arcarlson/Documents/RSS/Parks/SAGU/MACA Subset"
+MACADir = "E:/ClimateData/MACAv2Metdata" #no '/' at the end
+#Directory where output tables will be saved
+OutDir = "C:/Users/achildress/Documents/RSS/Working/FIIS/MACA/"
 
 #Variable and scenario names corresponding to MACA data directory structure
-vars = c("pr", "tasmax", "tasmin")
+vars = c("pr", "tasmax", "tasmin","rhsmax","rhsmin") #for typical RSS, don't need rhsmin/rhsmax
 scens = c("historical", "rcp45", "rcp85")
 
 #Variable names for output tables
-VarNames = c("PrecipCustom", "TmaxCustom", "TminCustom")
+VarNames = c("PrecipCustom", "TmaxCustom", "TminCustom","RHmaxCustom","RHminCustom") #for typical RSS, don't need rhsmin/rhsmax
 
 #GCMs to be extracted
 GCMs = c('bcc-csm1-1','bcc-csm1-1-m','BNU-ESM','CanESM2','CCSM4','CNRM-CM5','CSIRO-Mk3-6-0',
@@ -42,8 +42,8 @@ GCMs = c('bcc-csm1-1','bcc-csm1-1-m','BNU-ESM','CanESM2','CCSM4','CNRM-CM5','CSI
          'MIROC5','MIROC-ESM','MIROC-ESM-CHEM','MRI-CGCM3','NorESM1-M')
 
 #Date ranges to be extracted
-Future_StartYear = 2025   #2006-2099
-Future_EndYear = 2055     #2006-2099
+Future_StartYear = 2006   #2006-2099
+Future_EndYear = 2099     #2006-2099
 Hist_StartYear = 1950     #1950-2005
 Hist_EndYear = 2005       #1950-2005
 
@@ -61,9 +61,6 @@ CFs = c("Warm Wet", "Hot Wet", "Central", "Warm Dry", "Hot Dry") #Use spaces and
 
 Year = 2040
 Range = 30
-
-#Directory where output tables will be saved
-OutDir = "C:/Users/arcarlson/Documents/RSS/Parks/SAGU"
 
 ######################### End user-defined initials ##############################
 
@@ -267,87 +264,4 @@ months=factor(c("January","February","March","April","May","June","July","August
 seasons=factor(c("Winter", "Spring", "Summer", "Fall"))
 levels(seasons)=seasons
 
-####Set Average values for all four weather variables, using all baseline years and all climate models
-BaseMeanPr = mean(Baseline_all$PrecipCustom)
-BaseMeanTmx = mean(Baseline_all$TmaxCustom)
-BaseMeanTmn = mean(Baseline_all$TminCustom)
-# BaseMeanWind = mean(Baseline_all$Wind)
-
-####Create Future/Baseline means data tables, with averages for all four weather variables, organized by GCM
-Future_Means = data.frame(aggregate(cbind(Future_all$PrecipCustom, Future_all$TmaxCustom, Future_all$TminCustom)
-                                    ~ Future_all$GCM, Future_all, mean))   # , Future_all$Wind
-names(Future_Means) = c("GCM", "PrecipCustom", "TmaxCustom", "TminCustom")    # , "Wind"
-Future_Means$TavgCustom = (Future_Means$TmaxCustom + Future_Means$TminCustom)/2
-
-Baseline_Means = data.frame(aggregate(cbind(Baseline_all$PrecipCustom, Baseline_all$TmaxCustom, Baseline_all$TminCustom)
-                                      ~ Baseline_all$GCM, Baseline_all, mean))    #  ,Baseline_all$Wind)
-names(Baseline_Means) = c("GCM", "PrecipCustom", "TmaxCustom", "TminCustom")  #  , "Wind")
-Baseline_Means$TavgCustom = (Baseline_Means$TmaxCustom + Baseline_Means$TminCustom)/2
-
-#### add delta columns in order to classify CFs
-Future_Means$DeltaPr = Future_Means$PrecipCustom - Baseline_Means$PrecipCustom
-Future_Means$DeltaTmx = Future_Means$TmaxCustom - Baseline_Means$TmaxCustom
-Future_Means$DeltaTmn = Future_Means$TminCustom - Baseline_Means$TminCustom
-# Future_Means$DeltaWind = Future_Means$Wind - Baseline_Means$Wind
-Future_Means$DeltaTavg = Future_Means$TavgCustom - Baseline_Means$TavgCustom
-
-
-#### Set limits for CF classification
-Pr0 = as.numeric(quantile(Future_Means$DeltaPr, 0))
-Pr25 = as.numeric(quantile(Future_Means$DeltaPr, CFLow))
-PrAvg = as.numeric(mean(Future_Means$DeltaPr))
-Pr75 = as.numeric(quantile(Future_Means$DeltaPr, CFHigh))
-Pr100 = as.numeric(quantile(Future_Means$DeltaPr, 1))
-Tavg0 = as.numeric(quantile(Future_Means$DeltaTavg, 0))
-Tavg25 = as.numeric(quantile(Future_Means$DeltaTavg, CFLow)) 
-Tavg = as.numeric(mean(Future_Means$DeltaTavg))
-Tavg75 = as.numeric(quantile(Future_Means$DeltaTavg, CFHigh))
-Tavg100 = as.numeric(quantile(Future_Means$DeltaTavg, 1))
-# Wind25 = as.numeric(quantile(Future_Means$DeltaWind, CFLow))
-# WindAvg = as.numeric(mean(Future_Means$DeltaWind))
-# Wind75 = as.numeric(quantile(Future_Means$DeltaWind, CFHigh))
-
-#### Designate Climate Future
-Future_Means$CF1 = as.numeric((Future_Means$DeltaTavg<Tavg & Future_Means$DeltaPr>Pr75) | Future_Means$DeltaTavg<Tavg25 & Future_Means$DeltaPr>PrAvg)
-Future_Means$CF2 = as.numeric((Future_Means$DeltaTavg>Tavg & Future_Means$DeltaPr>Pr75) | Future_Means$DeltaTavg>Tavg75 & Future_Means$DeltaPr>PrAvg)
-Future_Means$CF3 = as.numeric((Future_Means$DeltaTavg>Tavg25 & Future_Means$DeltaTavg<Tavg75) & (Future_Means$DeltaPr>Pr25 & Future_Means$DeltaPr<Pr75))
-Future_Means$CF4 = as.numeric((Future_Means$DeltaTavg<Tavg & Future_Means$DeltaPr<Pr25) | Future_Means$DeltaTavg<Tavg25 & Future_Means$DeltaPr<PrAvg)
-Future_Means$CF5 = as.numeric((Future_Means$DeltaTavg>Tavg & Future_Means$DeltaPr<Pr25) | Future_Means$DeltaTavg>Tavg75 & Future_Means$DeltaPr<PrAvg)
-
-#Assign full name of climate future to new variable CF
-Future_Means$CF[Future_Means$CF1==1]=CFs[1]
-Future_Means$CF[Future_Means$CF2==1]=CFs[2]
-Future_Means$CF[Future_Means$CF3==1]=CFs[3]
-Future_Means$CF[Future_Means$CF4==1]=CFs[4]
-Future_Means$CF[Future_Means$CF5==1]=CFs[5]
-Future_Means$CF=as.factor(Future_Means$CF)
-Future_Means$CF = factor(Future_Means$CF,ordered=TRUE,levels=CFs)
-
-#     Remove extraneous Climate Future columns
-Future_Means$CF1 = NULL
-Future_Means$CF2 = NULL
-Future_Means$CF3 = NULL
-Future_Means$CF4 = NULL
-Future_Means$CF5 = NULL
-
-#     Add column with emissions scenario for each GCM run
-Future_Means$emissions[grep("rcp85",Future_Means$GCM)] = "RCP 8.5"
-Future_Means$emissions[grep("rcp45",Future_Means$GCM)] = "RCP 4.5"
-# Future_Means$emissions[grep("sresb1",Future_Means$GCM)] = "B1"
-
-####Order by date and GCM
-Future_all = Future_all[order(Future_all$GCM, Future_all$Date), ]
-Baseline_all = Baseline_all[order(Baseline_all$GCM, Baseline_all$Date),]
-
-####Add column with CF classification to Future_all/Baseline_all
-CF_GCM = data.frame(GCM = Future_Means$GCM, CF = Future_Means$CF)
-Future_all = merge(Future_all, CF_GCM[1:2], by="GCM")
-Baseline_all$CF = "Historical"
-
-Baseline_all$Date = strptime(Baseline_all$Date, "%Y-%m-%d")
-Future_all$Date = strptime(Future_all$Date, "%Y-%m-%d")
-
-WD_plots = paste(OutDir, "Figs MACA", sep="/")
-if(dir.exists(WD_plots) == FALSE){
-  dir.create(WD_plots)
-}
+save.image(paste(SiteID,"_init_parsed.RData",sep=""))
