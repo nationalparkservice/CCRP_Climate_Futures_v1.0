b700943323919d7eb8538cc3cf36edb90766bb34
Amber Childress-Runyon <acrunyon@users.noreply.github.com>
2020-01-21 15:12:05 -0700

    Add files via upload

--- Climate-Futures/RSS_MACA_Parsing.R
+++ Climate-Futures/RSS_MACA_Parsing.R
@@ -1,41 +1,48 @@
+####
+# Note to ACR - Need to rename var in bcc-csm1-1-m and add it back into GCMs list
+
+
+
 #######################################################################################
-# Script for extracting MACA data from NetCDF files and formatting data for RSS plots
-# Inputs: NetCDF files containing MACA-downscaled GCM projections, coordinates of desired grid cell, vector of desired GCMs, and year range to be extracted.
-# Outputs: Daily time series of pr, tasmax, and tasmin, contained in Baseline_all and Future_all tables 
+# Script for extracting historic and future projection data from daily CONUS MACAv2 dataset
+# Inputs: lat/lon coordinates, directory of .ncdf4 MACA data files, selected GCMs, and historical/future start and end years
+# Outputs: List of data frames containing all daily values for each scenario and variable, with data columns separated by GCM 
+#   Converts mm to inches and Kelvins to degrees F
 #######################################################################################
 
-library(RColorBrewer)
-library(ggplot2)
-library(ggmap)
-library(matrixStats)
-library(reshape)
-library(plyr)
-library(WriteXLS)
+#v01 - Running - Possible embellishment to include multiple grid cell parsing
+
 library(ncdf4)
+library(reshape2)
+
+rm(list=ls())
 
 ############################# User-defined initials #################################
 
 ####Location for data extraction
-# Lat and lon coordinates - Cell resolution is 0.041666 degrees, reference coordinates are cell centers
-Lat = 32.196
-Lon = -110.525
-cLon <- Lon + 360 #Adjusts negative lon. values 
-SiteID
+SiteID = "SCBL"
+
+#Cell resolution is 0.041666 degrees, reference coordinates are cell centers
+# YOSE_Valley  37.7460, -119.5880
+# YOSE_Tuolumne 37.8746, -119.3672
+
+Lat = 41.83476
+Lon = -103.707
+cLon = Lon + 360 #Adjusts negative lon. values 
 
 ###Parameters for extracting data from NetCDF files
 #Specify top-level directory where MACA data is stored (all scenarios and variables)
 MACADir = "E:/ClimateData/MACAv2Metdata" #no '/' at the end
-#Directory where output tables will be saved
-OutDir = "C:/Users/achildress/Documents/RSS/Working/FIIS/MACA/"
+setwd("C:/Users/achildress/Documents/RSS/Working/SCBL/MACA/")
 
 #Variable and scenario names corresponding to MACA data directory structure
-vars = c("pr", "tasmax", "tasmin","rhsmax","rhsmin") #for typical RSS, don't need rhsmin/rhsmax
+vars = c("pr", "tasmax", "tasmin","rhsmax","rhsmin")
 scens = c("historical", "rcp45", "rcp85")
 
 #Variable names for output tables
-VarNames = c("PrecipCustom", "TmaxCustom", "TminCustom","RHmaxCustom","RHminCustom") #for typical RSS, don't need rhsmin/rhsmax
+VarNames = c("PrecipCustom", "TmaxCustom", "TminCustom","RHmaxCustom","RHminCustom")
 
-#GCMs to be extracted
+# GCMs to be extracted
 GCMs = c('bcc-csm1-1','bcc-csm1-1-m','BNU-ESM','CanESM2','CCSM4','CNRM-CM5','CSIRO-Mk3-6-0',
          'GFDL-ESM2G','GFDL-ESM2M','HadGEM2-CC365','HadGEM2-ES365',
          'inmcm4','IPSL-CM5A-MR','IPSL-CM5A-LR','IPSL-CM5B-LR',
@@ -43,9 +50,13 @@ GCMs = c('bcc-csm1-1','bcc-csm1-1-m','BNU-ESM','CanESM2','CCSM4','CNRM-CM5','CSI
 
 #Date ranges to be extracted
 Future_StartYear = 2006   #2006-2099
-Future_EndYear = 2099     #2006-2099
+Future_EndYear = 2099   #2006-2099
 Hist_StartYear = 1950     #1950-2005
-Hist_EndYear = 2005       #1950-2005
+Hist_EndYear = 2005      #1950-2005
+HistYears = 50  
+
+#Directory where output tables will be saved
+OutDir = "C:/Users/achildress/Documents/RSS/Working/SCBL/MACA/"
 
 ######################### End user-defined initials ##############################
 
@@ -57,7 +68,7 @@ Hist_StartDate <- as.Date(paste(Hist_StartYear, "01", "01", sep="-"))
 Hist_EndDate <- as.Date(paste(Hist_EndYear, "12", "31", sep="-"))
 Hist_DateRange <- seq.Date(Hist_StartDate, Hist_EndDate, by="1 day")
 Future_StartDate <- as.Date(paste(Future_StartYear, "01", "01", sep="-"))
-Future_EndDate <- as.Date(paste(Future_EndYear, "12", "31", sep="-"))
+Future_EndDate <- as.Date(paste(Future_EndYear, "01", "01", sep="-"))
 Future_DateRange <- seq.Date(Future_StartDate, Future_EndDate, by="1 day")
 
 #Will store data frames of time series extracted for each scenario and variable. 
@@ -113,7 +124,7 @@ for(scen in scens){
     GCM <- strsplit(nc.files[1], "_")[[1]][3]
     
     #Loop through data and separate by GCM 
-    Dates <- as.Date(nc1$dim$time$vals, origin = "1970-01-01")
+    Dates <- as.Date(nc1$dim$time$vals, origin = "1900-01-01")
     Extr <- ncvar_get(nc1, varName, c(Lon_index, Lat_index, 1), count=c(1,1,-1))   
     GCM_data <- data.frame(Dates, Extr)
     nc_close(nc1)
@@ -121,11 +132,16 @@ for(scen in scens){
       GCM.file <- strsplit(nc.files[i], "_")[[1]][3]
       #Extract data
       nc <- nc_open(paste(varDir, nc.files[i], sep="/"))
-      Dates <- as.Date(nc$dim$time$vals, origin="1970-01-01")
+      varName <- names(nc$var)
+      Dates <- as.Date(nc$dim$time$vals, origin="1900-01-01")
       Extr <- ncvar_get(nc, varName, c(Lon_index, Lat_index, 1), count=c(1,1,-1))   
       Extr_DF <- data.frame(Dates, Extr)
       #Add each data series to separate GCM column within scen/var data frame
-      if(GCM.file != GCM){
+      if(GCM.file == GCM){
+        ##Extract data array and append to data frame
+        GCM_data <- rbind(GCM_data, Extr_DF)       
+      }
+      else{
         print(paste(GCM, "data extracted"))
         #Append extracted data series to data frame
         ScenVarDF <- merge(ScenVarDF, GCM_data, by="Dates")
@@ -140,10 +156,6 @@ for(scen in scens){
         #Begin new data series 
         GCM_data <- data.frame(Dates, Extr)
         GCM <- GCM.file
-      }
-      else{
-        ##Extract data array and append to data frame
-        GCM_data <- rbind(GCM_data, Extr_DF)  
       }
       nc_close(nc)
     }
@@ -175,7 +187,6 @@ for(scen in scens){
 ### End data extraction ###
 
 ##################################################################################
-
 #####Baseline_all - Daily time series for all GCMs for the historical period
 
 ### Summarize daily data ###
@@ -190,7 +201,7 @@ for(i in 2:length(vars)){
   df <- Hist_Data[[i]]
   df.melt <- melt(df, id="Dates")
   colnames(df.melt) <- c("Date", "GCM", VarNames[i])
-  Baseline_all <- merge(Baseline_all, df.melt, by=c("Date", "GCM"))
+  Baseline_all <- merge(Baseline_all, df.melt, by=c("Date", "GCM"),all=T)
 }
 
 #Future
@@ -218,29 +229,30 @@ for(i in 2:length(vars)){
   Future_all <- merge(Future_all, df, by=c("Date", "GCM"))
 }
 
-RCP45 <- melt(RCP45_Data[[1]], id="Dates")
-colnames(RCP45) <- c("Date", "GCM", VarNames[1])
-for(i in 2:length(RCP45_Data)){
-  df <- RCP45_Data[[i]]
-  df.melt <- melt(df, id="Dates")
-  RCP45 <- cbind(RCP45, df.melt$value)
-  colnames(RCP45)[ncol(RCP45)] <- VarNames[i]
-}
-RCP45$GCM <- paste(RCP45$GCM, "rcp45", sep=".")
+# RCP45 <- melt(RCP45_Data[[1]], id="Dates")
+# colnames(RCP45) <- c("Date", "GCM", VarNames[1])
+# for(i in 2:length(RCP45_Data)){
+#   df <- RCP45_Data[[i]]
+#   df.melt <- melt(df, id="Dates")
+#   colnames(df.melt) <- c("Date", "GCM", VarNames[i])
+#   RCP45 <- merge(RCP45, df.melt, by=c("Date", "GCM"),all=T)
+#   colnames(RCP45)[ncol(RCP45)] <- VarNames[i]
+# }
+# RCP45$GCM <- paste(RCP45$GCM, "rcp45", sep=".")
+# 
+# RCP85 <- melt(RCP85_Data[[1]], id="Dates")
+# colnames(RCP85) <- c("Date", "GCM", VarNames[1])
+# for(i in 2:length(RCP85_Data)){
+#   df <- RCP85_Data[[i]]
+#   colnames(df.melt) <- c("Date", "GCM", VarNames[i])
+#   RCP85 <- merge(RCP85, df.melt, by=c("Date", "GCM"),all=T)
+#   colnames(RCP85)[ncol(RCP85)] <- VarNames[i]
+# }
+# RCP85$GCM <- paste(RCP85$GCM, "rcp85", sep=".")
+# 
+# Future_all <- rbind(RCP45, RCP85)
 
-RCP85 <- melt(RCP85_Data[[1]], id="Dates")
-colnames(RCP85) <- c("Date", "GCM", VarNames[1])
-for(i in 2:length(RCP85_Data)){
-  df <- RCP85_Data[[i]]
-  df.melt <- melt(df, id="Dates")
-  RCP85 <- cbind(RCP85, df.melt$value)
-  colnames(RCP85)[ncol(RCP85)] <- VarNames[i]
-}
-RCP85$GCM <- paste(RCP85$GCM, "rcp85", sep=".")
-
-Future_all <- rbind(RCP45, RCP85)
-
-rm(DataExtraction, RCP45_Data, RCP85_Data, RCP45, RCP85)
+rm(DataExtraction, RCP45_Data, RCP85_Data)
 
 Baseline_all$TavgCustom <- (Baseline_all$TmaxCustom + Baseline_all$TminCustom)/2
 Future_all$TavgCustom <- (Future_all$TmaxCustom + Future_all$TminCustom)/2
@@ -250,3 +262,4 @@ seasons=factor(c("Winter", "Spring", "Summer", "Fall"))
 levels(seasons)=seasons
 
 save.image(paste(SiteID,"_init_parsed.RData",sep=""))
+
