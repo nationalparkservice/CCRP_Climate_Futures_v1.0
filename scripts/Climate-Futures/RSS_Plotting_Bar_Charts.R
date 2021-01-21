9f654dfa84dbbb7eb031fd4a4183ad31e45bfe43
Annie Kellner <annie.kellner@colostate.edu>
2021-01-21 12:35:22 -0700

    reconciled hist and fut df's and added new GDD variables

--- scripts/Climate-Futures/RSS_Plotting_Bar_Charts.R
+++ scripts/Climate-Futures/RSS_Plotting_Bar_Charts.R
@@ -4,7 +4,7 @@
 
 # Need to check all the subsets
 ### NEED TO CHANGE LINE 72 for ordering scenarios on plots
-FutureSubset <- c("Warm Wet","Hot Dry")          # Select two scenarios from the CFs vector specified in CMIP5_Parsing script. Names must match.
+
 Scenario1<-FutureSubset[1]
 Scenario2<-FutureSubset[2]
 
@@ -64,8 +64,8 @@ Season_delta$season<-factor(Season_delta$season,levels=c("Winter","Spring","Summ
 H<-H_annual[,-c(1:2)]
 Hist_annual<-aggregate(.~Year,data=H,mean);rm(H)
 Hist_annual$CF<-"Historical"
-Hist_annual<-Hist_annual[,c("Year","CF",names(Hist_annual[,2:23]))] 
-F_annual<-subset(F_annual, CF %in% FutureSubset, select= -c(GCM)) 
+Hist_annual<-Hist_annual[,c("Year","CF",names(Hist_annual[,2:26]))] 
+F_annual<-subset(F_annual, CF %in% FutureSubset, select = -c(GCM))
 Fut_annual<-aggregate(.~Year+CF,F_annual,mean)
 Annual<-rbind(Hist_annual,Fut_annual)
 Annual$CF<-factor(Annual$CF,levels=c("Historical",Scenario1, Scenario2), ordered=is.ordered(Annual$CF))
