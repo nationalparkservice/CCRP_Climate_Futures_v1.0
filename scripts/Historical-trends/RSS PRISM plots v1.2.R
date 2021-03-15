72b64ba7b980cd7895c1b89da0e81d681c38b03e
msears <megan_sears@partner.nps.gov>
2021-03-15 11:25:37 -0600

    switched regressions to come before annual average regression plots and automated dashed v solid lines added in

--- scripts/Historical-trends/RSS PRISM plots v1.2.R
+++ scripts/Historical-trends/RSS PRISM plots v1.2.R
@@ -260,93 +260,7 @@ OFName = paste("./figures/Historical-trends/", PlotName, " ", SiteID, " ", Lat, 
 ggsave(OFName, width=6.5, height=8.5, dpi=dpi)
 
 ##########################
-
-
-#-----------------------------------------------------------#
-#            ANNUAL AVERAGE LINES WITH REGRESSION           #
-#-----------------------------------------------------------#
-		#    ggplot graphics    #
-		# annaul points, linear regression, and 95% CI
-		# need to manually set position of a, b, c labels
-
-		# gray zone is 95% confidence interval
-
-PlotName = "Annual Means Lines Regressions"
-
-a <- ggplot(yrAvgs) +		#  attach only data frame in ggplot call
-			geom_smooth(method = lm, aes(cYr, tmaxAvg), na.rm=TRUE) +
-			geom_line(aes(cYr, tmaxAvg), na.rm=TRUE) + geom_point(aes(cYr, tmaxAvg), na.rm=TRUE) +
-      ylab(expression(paste(Tmax, ~({}^o*F)))) + xlab("") +
-      scale_x_continuous(breaks=c(1900, 1920, 1940, 1960, 1980, 2000))
-          #	geom_line(aes(cYr, rTmax), colour = 'red', size=1)}  # rolling mean
-
-      if(doP1 == "YES")a <- a + geom_smooth(method = lm, aes(cYr, tmaxP1), na.rm=TRUE)
-      if(doP2 == "YES")a <- a + geom_smooth(method = lm, aes(cYr, tmaxP2), na.rm=TRUE)
-
-	
-b <- ggplot(data=yrAvgs) + geom_line(aes(cYr, tminAvg), na.rm=TRUE) + geom_point(aes(cYr, tminAvg), na.rm=TRUE) +
-		ylab(expression(paste(Tmin, ~({}^o*F)))) + xlab("") +
-		# geom_text(aes(x=1895, y= 13.5, label = "B")) +
-		geom_smooth(aes(cYr, tminAvg), method="lm", na.rm=TRUE) +
-    scale_x_continuous(breaks=c(1900, 1920, 1940, 1960, 1980, 2000))
-        # geom_line(aes(cYr, rTmin), colour = 'blue', size=1)
-
-      if(doP1 == "YES")(b <- b +	geom_smooth(method = lm, aes(cYr, tminP1), na.rm=TRUE))
-			if(doP2 == "YES")b <- b +	geom_smooth(method = lm, aes(cYr, tminP2), na.rm=TRUE) 
-						
-c <- ggplot(data=yrAvgs) + geom_line(aes(cYr, tmeanAvg), na.rm=TRUE) + geom_point(aes(cYr, tmeanAvg), na.rm=TRUE) +
-		ylab(expression(paste(Tmean, ~({}^o*F)))) + xlab("") +
-		# geom_text(aes(x=1895, y= 13.5, label = "B")) +
-		geom_smooth(aes(cYr, tmeanAvg), method="lm", na.rm=TRUE) +
-		    # geom_line(aes(cYr, rTmean), colour = 'black', size=1) 
-    scale_x_continuous(breaks=c(1900, 1920, 1940, 1960, 1980, 2000))
-
-    if(doP1 == "YES")c <- c + geom_smooth(method = lm, aes(cYr, tmeanP1), na.rm=TRUE) 	
-    if(doP2 == "YES") c <- c + geom_smooth(method = lm, aes(cYr, tmeanP2), na.rm=TRUE) 
-
-d <- ggplot(data=yrAvgs) + geom_line(aes(cYr, pptAvg), na.rm=TRUE) + geom_point(aes(cYr, pptAvg), na.rm=TRUE) +
-    ylab("Precip (in/yr)") + xlab("") +
-		# geom_text(aes(x=1895, y=350, label = "C")) +
-		geom_smooth(aes(cYr, pptAvg), method="lm", na.rm=TRUE) +
-		    #geom_line(aes(cYr, rPpt), colour = 'green', size=1) 
-    scale_x_continuous(breaks=c(1900, 1920, 1940, 1960, 1980, 2000))
-
-    if(doP1 == "YES")d <- d + geom_smooth(method = lm, aes(cYr, pptP1), na.rm=TRUE)
-    if(doP2 == "YES")d <- d + geom_smooth(method = lm, aes(cYr, pptP2), na.rm=TRUE) 
-
-#4-panel plot		
-p1 = plot_grid(a, b, c, d, nrow=4, align="v")
-title = ggdraw() + draw_label(paste(SiteID, " - Trends for Reference and Recent \nHistorical Periods", sep=""), 
-                              fontface="bold", size=TitleSize, vjust=0.5)
-p2 = plot_grid(title, p1, ncol=1, rel_heights = c(0.07, 1)) 
-p3 = add_sub(p2, paste("Gray shaded area around regression lines = standard error of predicted y's \nReference period: ", beginRefYr, "-", endRefYr, "; Recent period: ", endRefYr+1, "-", EndYr, "; Overall period: ", BeginYr, "-", EndYr, sep=""),
-             y=.5, hjust=0.5, vjust=0.5, size=12)
-ggdraw(p3)
-
-OFName <- paste("./figures/Historical-trends/", PlotName, " 4-panel ", SiteID, " ", Lat, " ", Lon, ".png", sep = "")
-ggsave(OFName, width=6.5, height=8.5, dpi=dpi)
-
-#2-panel Tmax/Tmin plot
-p1 = plot_grid(a, b, nrow=2, align="v")
-p2 = plot_grid(title, p1, ncol=1, rel_heights = c(0.1, 1, 0.05)) 
-p3 = add_sub(p2, paste("Gray shaded area around regression lines = standard error of predicted y's \nReference period: ", beginRefYr, "-", endRefYr, "; Recent period: ", endRefYr+1, "-", EndYr, "; Overall period: ", BeginYr, "-", EndYr, sep=""),
-             y=.5, hjust=0.5, vjust=0.5, size=12)
-ggdraw(p3)
-
-OFName <- paste('./figures/Historical-trends/', PlotName, " Tmin Tmax ", SiteID, " ", Lat, " ", Lon, ".png", sep = "")
-ggsave(OFName, width=6.5, height=6.5, dpi=dpi)
-
-#2-panel Tmean/Precip plot
-p1 = plot_grid(c, d, nrow=2, align="v")
-p2 = plot_grid(title, p1, ncol=1, rel_heights = c(0.1, 1, 0.05)) 
-p3 = add_sub(p2, paste("Gray shaded area around regression lines = standard error of predicted y's \nReference period: ", beginRefYr, "-", endRefYr, "; Recent period: ", endRefYr+1, "-", EndYr, "; Overall period: ", BeginYr, "-", EndYr, sep=""),
-             y=.5, hjust=0.5, vjust=0.5, size=12)
-ggdraw(p3)
-
-OFName <- paste('./figures/Historical-trends/', PlotName, " Tmean Precip ", SiteID, " ", Lat, " ", Lon, ".png", sep = "")
-ggsave(OFName, width=6.5, height=6.5, dpi=dpi)
-
-		# regressions for trends
+# regressions for trends
 lmTmax <- lm(yrAvgs$tmaxAvg~cYr)
 lmTmaxP1 <- lm(yrAvgs$tmaxP1~cYr)
 lmTmaxP2 <- lm(yrAvgs$tmaxP2~cYr)
@@ -363,7 +277,7 @@ lmPpt  <- lm(yrAvgs$pptAvg~cYr)
 lmPptP1 <- lm(yrAvgs$pptP1~cYr)		
 lmPptP2 <- lm(yrAvgs$pptP2~cYr)		
 
-    # make table of coefficients
+# make table of coefficients
 probStar <- function(pVal){
   probStar <- "NS"
   if(pVal < 0.05)probStar <- "*"
@@ -399,16 +313,123 @@ lmTable <- cbind( Var=rep(c("Tmax", "Tmin", "Tmean", "Precip"),each=3), Period, 
 
 lmTable$YrCoeff <- lmTable$YrCoeff * 100   # convert to degF(in)/100-yrs
 lmTable$seSlope <- lmTable$seSlope * 100
-  #add units to YrCoeff field
+#add units to YrCoeff field
 colnames(lmTable) <- c("Var", "Period", "YrCoeff(degF(in)/100yrs)", "seSlope", "probCoeff", "probSign", "r2")
 
 print(lmTable, row.names = F)
 
 write.csv(lmTable, paste('./data/park-specific/output/', SiteID, " Regression Table test ", Sys.Date(), ".csv", 
-      sep=""), row.names=FALSE)
+                         sep=""), row.names=FALSE)
+
+#-----------------------------------------------------------#
+#            ANNUAL AVERAGE LINES WITH REGRESSION           #
+#-----------------------------------------------------------#
+# Amend to incl. running means and dashed lines for significance
+#    ggplot graphics    #
+# annaul points, linear regression, and 95% CI
+# need to manually set position of a, b, c labels
+
+# gray zone is 95% confidence interval
+
+PlotName = "Annual Means Lines Regressions"
+
+a <- ggplot(yrAvgs) +	geom_smooth(method = lm, aes(cYr, tmaxAvg), na.rm=TRUE,linetype=if(summary(lmTmax)$coefficients[2,4]<0.05) {
+  1
+} else{2}) +
+  geom_line(aes(cYr, tmaxAvg), na.rm=TRUE) + geom_point(aes(cYr, tmaxAvg), na.rm=TRUE) +
+  ylab(expression(paste(Tmax, ~({}^o*F)))) + xlab("") +
+  scale_x_continuous(breaks=c(1900, 1920, 1940, 1960, 1980, 2000)) +
+  geom_line(aes(cYr, rTmax), colour = 'brown', size=1)  # rolling mean
+if(doP1 == "YES")a <- a + geom_smooth(method = lm, aes(cYr, tmaxP1), na.rm=TRUE,linetype=if(summary(lmTmaxP1)$coefficients[2,4]<0.05) {
+  1
+} else{2})
+if(doP2 == "YES")a <- a + geom_smooth(method = lm, aes(cYr, tmaxP2), na.rm=TRUE,linetype=if(summary(lmTmaxP2)$coefficients[2,4]<0.05) {
+  1
+} else{2})
+a
+
+b <- ggplot(data=yrAvgs) + geom_line(aes(cYr, tminAvg), na.rm=TRUE) + geom_point(aes(cYr, tminAvg), na.rm=TRUE) +
+  ylab(expression(paste(Tmin, ~({}^o*F)))) + xlab("") +
+  # geom_text(aes(x=1895, y= 13.5, label = "B")) +
+  geom_smooth(aes(cYr, tminAvg), method="lm", na.rm=TRUE,linetype=if(summary(lmTmin)$coefficients[2,4]<0.05) {
+    1
+  } else{2}) +
+  scale_x_continuous(breaks=c(1900, 1920, 1940, 1960, 1980, 2000))+
+  geom_line(aes(cYr, rTmin), colour = 'brown', size=1)
+
+if(doP1 == "YES")b <- b +	geom_smooth(method = lm, aes(cYr, tminP1), na.rm=TRUE,linetype=if(summary(lmTminP1)$coefficients[2,4]<0.05) {
+  1
+} else{2})
+if(doP2 == "YES")b <- b +	geom_smooth(method = lm, aes(cYr, tminP2), na.rm=TRUE,linetype=if(summary(lmTminP2)$coefficients[2,4]<0.05) {
+  1
+} else{2})
+
+c <- ggplot(data=yrAvgs) + geom_line(aes(cYr, tmeanAvg), na.rm=TRUE) + geom_point(aes(cYr, tmeanAvg), na.rm=TRUE) +
+  ylab(expression(paste(Tmean, ~({}^o*F)))) + xlab("") +
+  # geom_text(aes(x=1895, y= 13.5, label = "B")) +
+  geom_smooth(aes(cYr, tmeanAvg), method="lm", na.rm=TRUE,linetype=if(summary(lmTmean)$coefficients[2,4]<0.05) {
+    1
+  } else{2}) +
+  geom_line(aes(cYr, rTmean), colour = 'brown', size=1) +
+  scale_x_continuous(breaks=c(1900, 1920, 1940, 1960, 1980, 2000))
+
+if(doP1 == "YES")c <- c + geom_smooth(method = lm, aes(cYr, tmeanP1), na.rm=TRUE,linetype=if(summary(lmTmeanP1)$coefficients[2,4]<0.05) {
+  1
+} else{2})
+if(doP2 == "YES") c <- c + geom_smooth(method = lm, aes(cYr, tmeanP2), na.rm=TRUE,linetype=if(summary(lmTmeanP2)$coefficients[2,4]<0.05) {
+  1
+} else{2}) 
+
+d <- ggplot(data=yrAvgs) + geom_line(aes(cYr, pptAvg), na.rm=TRUE) + geom_point(aes(cYr, pptAvg), na.rm=TRUE) +
+  ylab("Precip (in/yr)") + xlab("") +
+  # geom_text(aes(x=1895, y=350, label = "C")) +
+  geom_smooth(aes(cYr, pptAvg), method="lm", na.rm=TRUE,linetype=if(summary(lmPpt)$coefficients[2,4]<0.05) {
+    1
+  } else{2}) +
+  geom_line(aes(cYr, rPpt), colour = 'brown', size=1) +
+  scale_x_continuous(breaks=c(1900, 1920, 1940, 1960, 1980, 2000))
+
+if(doP1 == "YES")d <- d + geom_smooth(method = lm, aes(cYr, pptP1), na.rm=TRUE,linetype=if(summary(lmPptP1)$coefficients[2,4]<0.05) {
+  1
+} else{2})
+if(doP2 == "YES")d <- d + geom_smooth(method = lm, aes(cYr, pptP2), na.rm=TRUE,linetype=if(summary(lmPptP2)$coefficients[2,4]<0.05) {
+  1
+} else{2}) 
 
 rm(lmPpt,lmPptP1,lmPptP2,lmTmax,lmTmaxP1,lmTmaxP2,lmTmin,lmTminP1,lmTminP2,lmTmean,lmTmeanP1,lmTmeanP2)
 rm(regsTmax, regsTmin, regsTmean, regsPpt, lmTable)
+
+#4-panel plot		
+p1 = plot_grid(a, b, c, d, nrow=4, align="v")
+title = ggdraw() + draw_label(paste(SiteID, " - Trends for Reference and Recent \nHistorical Periods", sep=""), 
+                              fontface="bold", size=TitleSize, vjust=0.5)
+p2 = plot_grid(title, p1, ncol=1, rel_heights = c(0.07, 1)) 
+p3 = add_sub(p2, paste("Gray shaded area around regression lines = standard error of predicted y's \nReference period: ", beginRefYr, "-", endRefYr, "; Recent period: ", endRefYr+1, "-", EndYr, "; Overall period: ", BeginYr, "-", EndYr, sep=""),
+             y=.5, hjust=0.5, vjust=0.5, size=12)
+ggdraw(p3)
+
+OFName <- paste("./figures/Historical-trends/", PlotName, " 4-panel ", SiteID, " ", Lat, " ", Lon, ".png", sep = "")
+ggsave(OFName, width=6.5, height=8.5, dpi=dpi)
+
+#2-panel Tmax/Tmin plot
+p1 = plot_grid(a, b, nrow=2, align="v")
+p2 = plot_grid(title, p1, ncol=1, rel_heights = c(0.1, 1, 0.05)) 
+p3 = add_sub(p2, paste("Gray shaded area around regression lines = standard error of predicted y's \nReference period: ", beginRefYr, "-", endRefYr, "; Recent period: ", endRefYr+1, "-", EndYr, "; Overall period: ", BeginYr, "-", EndYr, sep=""),
+             y=.5, hjust=0.5, vjust=0.5, size=12)
+ggdraw(p3)
+
+OFName <- paste('./figures/Historical-trends/', PlotName, " Tmin Tmax ", SiteID, " ", Lat, " ", Lon, ".png", sep = "")
+ggsave(OFName, width=6.5, height=6.5, dpi=dpi)
+
+#2-panel Tmean/Precip plot
+p1 = plot_grid(c, d, nrow=2, align="v")
+p2 = plot_grid(title, p1, ncol=1, rel_heights = c(0.1, 1, 0.05)) 
+p3 = add_sub(p2, paste("Gray shaded area around regression lines = standard error of predicted y's \nReference period: ", beginRefYr, "-", endRefYr, "; Recent period: ", endRefYr+1, "-", EndYr, "; Overall period: ", BeginYr, "-", EndYr, sep=""),
+             y=.5, hjust=0.5, vjust=0.5, size=12)
+ggdraw(p3)
+
+OFName <- paste('./figures/Historical-trends/', PlotName, " Tmean Precip ", SiteID, " ", Lat, " ", Lon, ".png", sep = "")
+ggsave(OFName, width=6.5, height=6.5, dpi=dpi)
 
 #-------------------------------------------------#
 #         Decadal PLOTS by "step" and season  #####
