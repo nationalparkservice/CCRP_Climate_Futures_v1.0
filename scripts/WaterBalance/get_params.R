45a2b84d9398a3b0030322fd74aa376d083965df
Annie Kellner <annie.kellner@colostate.edu>
2021-05-10 14:24:31 -0600

    Change from 'point' to 'id'

--- scripts/WaterBalance/get_params.R
+++ scripts/WaterBalance/get_params.R
@@ -127,7 +127,7 @@ sites[,10] <- 0 # default value for Soil.Init
 sites[,11] <- 1 # default value for shade coefficient
 
 sites <- select(sites, 7,2,1,3:6, 8:11) # reorder columns
-colnames(sites) <- c("Point", "Lat", "Lon", "Elev", "Aspect", "Slope", "SWC.Max", "Wind", "Snowpack", "Soil.Init", "Shade.Coeff")
+colnames(sites) <- c("ID", "Lat", "Lon", "Elev", "Aspect", "Slope", "SWC.Max", "Wind", "Snowpack", "Soil.Init", "Shade.Coeff")
 
 sites$SWC.Max = sites$SWC.Max*10 # convert units for Soil Water-holding capacity
 sites # check to be sure values are populated correctly. There should not be NA values. 
