dd75a704c9ef70ff5a6797b553dd6e89466dd22c
Annie Kellner <annie.kellner@colostate.edu>
2021-09-24 14:01:03 -0600

    Rewrite script so that Java is no longer necessary

--- scripts/Misc/create_site_maps.R
+++ scripts/Misc/create_site_maps.R
@@ -1,47 +1,63 @@
-########################################
-####  GENERATE SITE MAP   ##############
-########################################
 
-# This script generates a series of maps of the park, highlighting the single MACA cell selected for analysis. 
-# Multiple maps are created to accommodate differences in park sizes. Project lead can choose the map scaled most appropriately.
 
-# NOTE: It does not seem to work to overlay the park layer onto the osm map and add transparency. You need to overlay the osm onto the park layer
-# and make the osm layer transparent. 
 
-tmap_mode('plot')
+###### Map of MACA cell in context of the park  #############################
 
-# Maps with 25 MACA cells
-#"Directions" refers to the number of directions in which cells should be connected. 
-# 16 = knight and one-cell queen moves. See adjacent function for more info. 
+# Park
 
-adjacent_cells <- adjacent(maca, cells = cell, directions = 16) # Find cells around centroid. 
-adjacent_cells <- rasterFromCells(maca, adjacent_cells) # Create new raster from cells
-adjacent_poly <- rasterToPolygons(adjacent_cells) # Convert raster to polygon so cell outlines are visible
+park <- st_transform(park, 4326) # in order to use auto zoom feature, must be in lat/long
 
-adjacent_poly <- spTransform(adjacent_poly, CRSobj = "+init=epsg:4326")
+box = sf::st_bbox(park) # Get bbox before turning into sp object
+Sp_park= as(park, "Spatial")
 
-map_types = c("bing", "osm", "stamen-terrain", "esri-topo", "apple-iphoto") # map types can be found here: https://www.rdocumentation.org/packages/OpenStreetMap/versions/0.3.4/topics/openmap
+myMap <- get_stamenmap(bbox = c(left = Sp_park@bbox[1],
+                                bottom = Sp_park@bbox[2],
+                                right = Sp_park@bbox[3],
+                                top = Sp_park@bbox[4]),
+                       maptype = "terrain",
+                       crop = FALSE, zoom = calc_zoom(lat = c(box[2],box[4]),lon=c(box[1],box[3])))
+ggmap(myMap)
 
-# Loop that creates series of maps with 25 MACA cells
+# MACA grid
 
-for(i in 1:length(map_types)){
-  osm <- tmaptools::read_osm(bb(adjacent_poly), type = map_types[i])
-  map <- tm_shape(park) + tm_borders(col = "black", lwd = 3) + 
-    tm_fill(col = "darkgreen") + 
-    tm_shape(osm, is.master = TRUE) + tm_rgb(alpha = 0.8) +
-    tm_shape(adjacent_poly) + tm_borders(lwd = 3, col = "orange") + 
-    tm_shape(maca.poly, unit = "mi") + tm_borders(col = "#0000ff", lwd = 4) + 
-    tm_add_legend(type = "line", label = "MACA grid", col = "orange", lwd = 3) + 
-    tm_add_legend(type = "line", label = "Selected CMIP5 cell", col = "#0000ff", lwd = 4) +
-    tm_add_legend(type = "fill", label = "National Park", col = "darkgreen") +
-    tm_compass(type = "4star", position = c("right", "top"), size = 3, bg.color = "white") + tm_scale_bar(text.size = 0.75, breaks = c(0,1,2,3), bg.color = "white") +
-    tm_layout(legend.position = c("left", "bottom"), legend.stack = "vertical", legend.frame = TRUE, legend.bg.color = "white", legend.text.size = 1)
-  
-  tmap_save(map, filename = paste0(maps, "/", SiteID, "-MACA25-", map_types[i], ".png"))
-  
-}
+#maca_shp <- rasterToPolygons(maca) # Create new MACA shapefile that overlaps MACA raster - will add to spatial data on SHarepoint
 
-# Loop that creates maps with 9 MACA cells 
+maca_grid_shp <- st_read('./data/general/spatial-data/Climate_grid/MACA_grid.shp') 
+maca_grid_shp <- st_transform(maca_grid_shp, 4326)
+maca_grid_crop <- st_crop(maca_grid_shp, box)
+
+# MACA grid cell 
+maca.sf <- st_as_sf(maca.poly)
+maca.sf <- st_transform(maca.sf, 4326)
+
+ggmap(myMap, aes(x=x, y=y)) +
+  geom_sf(data = park, inherit.aes = FALSE, aes(color = "Park"), fill = NA,lwd=1) + 
+  geom_sf(data = maca_grid_crop, inherit.aes = FALSE, aes(color="MACA grid"), fill = NA, lwd=0.25) +
+  geom_sf(data = maca.sf, inherit.aes = FALSE,fill = NA,lwd= 1.5, aes(colour="Selected CMIP5 cell")) +
+  scale_color_manual(values = c("Park" = "black", 
+                                "MACA grid" = alpha("black", 0.25), 
+                                "Selected CMIP5 cell" = "orange")) + 
+  annotation_scale() + 
+  annotation_north_arrow(which_north = "True", 
+                         location = "tr", 
+                         height = unit(1, "cm"), 
+                         width = unit(1, "cm"), 
+                         pad_x = unit(0.25, "in"), 
+                         pad_y = unit(0.33, "in")) +
+  theme_classic() + 
+  theme(axis.line = element_blank(), 
+        axis.title = element_blank(), 
+        axis.text = element_blank(), 
+        axis.ticks = element_blank(),
+        legend.title = element_blank(),
+        legend.margin = margin(0,0,0,0),
+        legend.position = "bottom"
+        )
+
+
+ggsave(filename = paste0(SiteID, "-map-MACA-zoomed-out", ".png"), device = "png", path = './figures/maps')
+
+############    MACA cell zoomed-in   #############################################################################
 
 adjacent_cells <- adjacent(maca, cells = cell, directions = 8) # Find cells around centroid. 8 = "Queen's case"
 adjacent_cells <- rasterFromCells(maca, adjacent_cells) # Create new raster from cells
@@ -49,22 +65,41 @@ adjacent_poly <- rasterToPolygons(adjacent_cells) # Convert raster to polygon so
 
 adjacent_poly <- spTransform(adjacent_poly, CRSobj = "+init=epsg:4326")
 
-map_types = c("bing", "osm", "stamen-terrain", "esri-topo", "apple-iphoto")
+adjacent_poly_sf <- st_as_sf(adjacent_poly)
+box = sf::st_bbox(adjacent_poly) 
 
-for(i in 1:length(map_types)){
-  osm <- tmaptools::read_osm(bb(adjacent_poly), type = map_types[i])
-  map <- tm_shape(park, name = "National Park") + 
-    tm_borders() +
-    tm_fill(col = "darkgreen") + 
-    tm_shape(osm, is.master = TRUE) + tm_rgb(alpha = 0.5) +
-    tm_shape(adjacent_poly) + tm_borders(lwd = 3, col = "orange") + 
-    tm_shape(maca.poly, unit = "mi") + tm_borders(col = "#0000ff", lwd = 4) + 
-    tm_add_legend(type = "line", label = "MACA grid", col = "orange", lwd = 3) + 
-    tm_add_legend(type = "line", label = "Selected CMIP5 cell", col = "#0000ff", lwd = 4) +
-    tm_add_legend(type = "fill", label = "National Park", col = "darkgreen", alpha = 0.5) +
-    tm_compass(type = "4star", position = c("right", "top"), size = 3, bg.color = "white") + tm_scale_bar(text.size = 0.75, breaks = c(0,1,2,3), bg.color = "white") +
-    tm_layout(legend.position = c("left", "bottom"), legend.stack = "vertical", legend.frame = TRUE, legend.bg.color = "white", legend.text.size = 1)
+# Get bounding box and map
+
+myMap2 <- get_stamenmap(bbox = c(left = adjacent_poly@bbox[1],
+                                  bottom = adjacent_poly@bbox[2],
+                                  right = adjacent_poly@bbox[3],
+                                  top = adjacent_poly@bbox[4]),
+                         maptype = "terrain",
+                         crop = FALSE, zoom = calc_zoom(lat = c(box[2],box[4]),lon=c(box[1],box[3])))
+
+ggmap(myMap2, aes(x=x, y=y)) + 
+  geom_sf(data = adjacent_poly_sf, inherit.aes = FALSE, aes(color = "MACA grid"), fill = NA, lwd = 1) + 
+  geom_sf(data = maca.sf, inherit.aes = FALSE,fill = NA,lwd= 1.5, aes(colour="Selected CMIP5 cell")) +
+  scale_color_manual(values = c("MACA grid" = alpha("black", 0.5),
+                                "Selected CMIP5 cell" = "orange")) + 
+  annotation_scale() + 
+  annotation_north_arrow(which_north = "True", 
+                         location = "tr", 
+                         height = unit(1, "cm"), 
+                         width = unit(1, "cm"), 
+                         pad_x = unit(0.25, "in"), 
+                         pad_y = unit(0.33, "in")) +
+  theme_classic() + 
+  theme(axis.line = element_blank(), 
+        axis.title = element_blank(), 
+        axis.text = element_blank(), 
+        axis.ticks = element_blank(),
+        legend.title = element_blank(),
+        legend.margin = margin(0,0,0,0),
+        legend.position = "bottom"
+  )
   
-  tmap_save(map, filename = paste0(maps, "/", SiteID, "-MACA9-", map_types[i], ".png"))
-  
-}
+                                  
+ggsave(filename = paste0(SiteID, "-map-MACA-zoomed-in", ".png"), device = "png", path = './figures/maps')                         
+
+
