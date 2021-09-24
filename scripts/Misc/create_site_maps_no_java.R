b6494fe782a54b7c45049bc250f554fc0186b694
Annie Kellner <annie.kellner@colostate.edu>
2021-09-24 12:28:07 -0600

    park map complete

--- scripts/Misc/create_site_maps_no_java.R
+++ scripts/Misc/create_site_maps_no_java.R
@@ -1,5 +1,6 @@
 
 library(ggmap)
+library(ggspatial)
 
 ###### Map of MACA cell in context of the park  #############################
 
@@ -10,7 +11,7 @@ park <- st_transform(park, 4326) # in order to use auto zoom feature, must be in
 box = sf::st_bbox(park) # Get bbox before turning into sp object
 Sp_park= as(park, "Spatial")
 
-myMap <- get_stamenmap(bbox = c(left = Sp_park@bbox[1],
+myMap <- get_terrainmap(bbox = c(left = Sp_park@bbox[1],
                                 bottom = Sp_park@bbox[2],
                                 right = Sp_park@bbox[3],
                                 top = Sp_park@bbox[4]),
@@ -21,10 +22,10 @@ ggmap(myMap)
 # MACA grid
 
 #maca_shp <- rasterToPolygons(maca) # Create new MACA shapefile that overlaps MACA raster - will add to spatial data on SHarepoint
-#maca.shp <- st_read('./data/general/spatial-data/Climate_grid/MACA_grid.shp') 
-maca_grid_sf <- st_as_sf(maca_shp)
-maca_grid_sf <- st_transform(maca_grid_sf, 4326)
-maca_crop <- st_crop(maca_grid_sf, box)
+
+maca_grid_shp <- st_read('./data/general/spatial-data/Climate_grid/MACA_grid.shp') 
+maca_grid_shp <- st_transform(maca_grid_shp, 4326)
+maca_grid_crop <- st_crop(maca_grid_shp, box)
 
 # MACA grid cell 
 maca.sf <- st_as_sf(maca.poly)
@@ -32,11 +33,12 @@ maca.sf <- st_transform(maca.sf, 4326)
 
 ggmap(myMap, aes(x=x, y=y)) +
   geom_sf(data = park, inherit.aes = FALSE, aes(color = "Park"), fill = NA,lwd=1) + 
-  geom_sf(data = maca_crop, inherit.aes = FALSE, aes(color="MACA grid"), fill = NA, lwd=0.25) +
-  geom_sf(data = maca.sf, inherit.aes = FALSE,fill = NA,lwd=0.5, aes(colour="Selected cell")) +
+  geom_sf(data = maca_grid_crop, inherit.aes = FALSE, aes(color="MACA grid"), fill = NA, lwd=0.25) +
+  geom_sf(data = maca.sf, inherit.aes = FALSE,fill = NA,lwd= 1, aes(colour="Selected CMIP5 cell")) +
   scale_color_manual(values = c("Park" = "black", 
                                 "MACA grid" = alpha("black", 0.25), 
-                                "Selected cell" = "orange")) + 
+                                "Selected CMIP5 cell" = "orange")) + 
+  annotation_scale() + annotation_north_arrow(which_north = "True", location = "tr") +
   theme_classic() + 
   theme(axis.line = element_blank(), 
         axis.title = element_blank(), 
