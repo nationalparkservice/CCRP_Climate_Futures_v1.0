86e4953785d93d3649e06136b0a893f1cd04cbbd
acrunyon <amber.c.runyon@gmail.com>
2021-12-21 07:48:47 -0700

    Update Return Events script to run in RCF

--- scripts/Additional-tables-plots/Return_Events.R
+++ scripts/Additional-tables-plots/Return_Events.R
@@ -1,23 +1,5 @@
 ##### Testing return period / exceedance probability calculations
-library(ggplot2)
-library(plyr)
-
-rm(list=ls())
-
-#load daily data
-setwd("C:/Users/achildress/DOI/NPS-CCRP-FC Science Adaptation - Documents/General/RSS Stuff/Parks/MACA/Figs projections/")
-load("MACA_37.19758_-86.130895_Final_Environment.RData")
-rm(list=setdiff(ls(), c("Baseline_all","Future_all","SiteID")))
-
-#Use same GCMs/CFs used for drought / WB data
-GCMs = c("CNRM-CM5.rcp45","HadGEM2-ES365.rcp85") 
-CFs<- c("Warm Wet", "Hot Dry")
-
-## All 508-compliant color scheme -- navy (hot wet), light blue (warm wet), pink (warm dry), red (hot dry)
-colors2<- c("#9A9EE5","#E10720")  # WarmWet/HotDry
-#colors2<- c("#F3D3CB","#12045C")  # HotWet/WarmDry
-
-colors3<-c("dark grey",colors2)
+OutDir<-("./figures/additional")
 
 #Height and width 
 PlotWidth = 18
@@ -43,20 +25,19 @@ BarPlotTheme = theme(axis.text.x=element_text(size=24),    #Text size for axis t
 ############### Analysis on MACA data###########################
 
 ### CF1 future 
-model1<-GCMs[1]
-Future_all$Year<-format(Future_all$Date,"%Y")
-Future_model1<-subset(Future_all,GCM %in% model1 & Year >2024 & Year<2056)
+model1<-WB_GCMs$GCM[1]
+Future_all$Year=NULL
+Future_model1<-subset(Future_all,GCM %in% model1 & yr >= (Year-Range/2) & yr<= (Year+Range/2))
 
 ##CF1 baseline
-Baseline_all$Year<-format(Baseline_all$Date,"%Y")
 Baseline_model1<-subset(Baseline_all, GCM %in% model1 & Year<1999)
 
 # Annual max and plot for cF1
 Base1_max<-aggregate(PrecipCustom~Year+GCM,Baseline_model1,max)
-Future1_max<-aggregate(PrecipCustom~Year+GCM,Future_model1,max)
+Future1_max<-aggregate(PrecipCustom~yr+GCM,Future_model1,max)
 
 ggplot(Base1_max,aes(x=Year,y=PrecipCustom,colour=GCM)) + geom_point(aes(colour=GCM)) 
-ggplot(Future1_max,aes(x=Year,y=PrecipCustom,colour=GCM)) + geom_point(aes(colour=GCM)) 
+ggplot(Future1_max,aes(x=yr,y=PrecipCustom,colour=GCM)) + geom_point(aes(colour=GCM)) 
 
 Base1_GCM<-split(Base1_max,Base1_max$GCM) #Splits df into array by GCM
 Future1_GCM<-split(Future1_max,Future1_max$GCM)
@@ -76,8 +57,6 @@ Base1<-ldply(Base1_GCM,data.frame)
 Future1<-ldply(Future1_GCM,data.frame)
 
 ##CF1 baseline 20 year return
-ggplot(Base1,aes(x=return,y=PrecipCustom,colour=GCM)) + geom_point()
-
 regression<-lm(PrecipCustom~log(return),data=Base1)
 Base1$modeled<-predict(regression)
 
@@ -85,16 +64,10 @@ max100base1<-data.frame(return=seq(1,100,1))
 max100base1$modeled<-predict(regression,newdata=max100base1)
 max100base1$CF<-CFs[1]
 
-plot(Base1$return,Base1$PrecipCustom, "p", las=1, xlab="return", ylab="Precip",
-     xlim=c(0,100),ylim=c(0,max(max100base1$modeled)))
-lines(max100base1$modeled~max100base1$return, col="blue", lwd=2)
-
 return20base1<-data.frame(return=20)
 return20base1$mod<-predict(regression,newdata=return20base1)
 
 ##CF1 future 20 year return
-ggplot(Future1,aes(x=return,y=PrecipCustom,colour=GCM)) + geom_point()
-
 regression<-lm(PrecipCustom~log(return),data=Future1)
 Future1$modeled<-predict(regression)
 
@@ -102,32 +75,24 @@ max100future1<-data.frame(return=seq(1,100,1))
 max100future1$modeled<-predict(regression,newdata=max100future1)
 max100future1$CF<-CFs[1]
 
-plot(Future1$return,Future1$PrecipCustom, "p", las=1, xlab="return", ylab="Precip",
-     xlim=c(0,100),ylim=c(0,max(max100future1$modeled)))
-lines(max100future1$modeled~max100future1$return, col="blue", lwd=2)
-
 return20future1<-data.frame(return=20)
 return20future1$mod<-predict(regression,newdata=return20future1)
 return20future1$CF<-CFs[1]
 
 ##################################################################
 ######## CF2 future and baseline
-model2<-GCMs[2]
-Future_model2<-subset(Future_all,GCM %in% model2 & Year >2024 & Year<2056)
+model2<-WB_GCMs$GCM[2]
+Future_model2<-subset(Future_all,GCM %in% model2 & yr >= (Year-Range/2) & yr<= (Year+Range/2))
 
 Baseline_model2<-subset(Baseline_all, GCM %in% model2 & Year<1999)
 
 # Annual max and plot for cF2
 Base2_max<-aggregate(PrecipCustom~Year+GCM,Baseline_model2,max)
-Future2_max<-aggregate(PrecipCustom~Year+GCM,Future_model2,max)
-ggplot(Base2_max,aes(x=Year,y=PrecipCustom,colour=GCM)) + geom_point(aes(colour=GCM)) 
-ggplot(Future2_max,aes(x=Year,y=PrecipCustom,colour=GCM)) + geom_point(aes(colour=GCM)) 
-
+Future2_max<-aggregate(PrecipCustom~yr+GCM,Future_model2,max)
 
 Base2_GCM<-split(Base2_max,Base2_max$GCM) #Splits df into array by GCM
 Future2_GCM<-split(Future2_max,Future2_max$GCM)
 
-library(plyr)
 for (i in 1:length(model1)){
   name=names(Base2_GCM)[i]
   b2<-Base2_GCM[[i]]; f2<-Future2_GCM[[i]]
@@ -143,8 +108,6 @@ Base2<-ldply(Base2_GCM,data.frame)
 Future2<-ldply(Future2_GCM,data.frame)
 
 ##CF2 baseline 20 year return
-ggplot(Base2,aes(x=return,y=PrecipCustom,colour=GCM)) + geom_point()
-
 regression<-lm(PrecipCustom~log(return),data=Base2)
 Base2$modeled<-predict(regression)
 
@@ -152,26 +115,16 @@ max100base2<-data.frame(return=seq(1,100,1))
 max100base2$modeled<-predict(regression,newdata=max100base2)
 max100base2$CF<-CFs[2]
 
-plot(Base2$return,Base2$PrecipCustom, "p", las=1, xlab="return", ylab="Precip",
-     xlim=c(0,100),ylim=c(0,max(max100base2$modeled)))
-lines(max100base2$modeled~max100base2$return, col="blue", lwd=2)
-
 return20base2<-data.frame(return=20)
 return20base2$mod<-predict(regression,newdata=return20base2)
 
 ##CF2 future 20 year return
-ggplot(Future2,aes(x=log(return),y=PrecipCustom,colour=GCM)) + geom_point()
-
 regression<-lm(PrecipCustom~log(return),data=Future2)
 Future2$modeled<-predict(regression)
 
 max100future2<-data.frame(return=seq(1,100,1))
 max100future2$modeled<-predict(regression,newdata=max100future2)
 max100future2$CF<-CFs[2]
-
-plot(Future2$return,Future2$PrecipCustom, "p", las=1, xlab="return", ylab="Precip",
-     xlim=c(0,100),ylim=c(0,max(max100future2$modeled)))
-lines(max100future2$modeled~max100future2$return, col="blue", lwd=2)
 
 return20future2<-data.frame(return=20)
 return20future2$mod<-predict(regression,newdata=return20future2)
@@ -208,7 +161,7 @@ ggplot(allreturns, aes(x=CF, y=mod,fill=CF)) +
        x ="20-year recurrence interval", y ="Precipitation (inches/day)") +
   scale_fill_manual(name="",values = colors3) 
 
-ggsave("20yr-bar-plot.png", width = PlotWidth, height = PlotHeight)
+ggsave("20yr-bar-plot.png", path=OutDir, width = PlotWidth, height = PlotHeight)
 
 
 ######line plot of return int regressions
@@ -231,7 +184,6 @@ ggplot(allregressions, aes(x=return, y=modeled, group=CF, colour = CF)) +
   scale_fill_manual(name="",values = colors3) +
   scale_shape_manual(name="",values = c(21,22,23))
 
-ggsave("20yr-regressions.png", width = PlotWidth, height = PlotHeight)
+ggsave("20yr-regressions.png", path=OutDir, width = PlotWidth, height = PlotHeight)
 
-allregressions
 
