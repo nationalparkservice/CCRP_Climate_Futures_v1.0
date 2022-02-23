70f57e9b830dd921bb2dfca4d8e86fefa9c7445d
acrunyon <amber.c.runyon@gmail.com>
2022-02-23 10:18:50 -0700

    Move Plot parameters into own script

---
+++ scripts/Climate-Futures/Plotting_Functions.R
@@ -0,0 +1,22 @@
+##Plot parameters
+
+#X-axis labels for monthly plots
+MonthLabels = c("J", "F", "M", "A", "M", "J", "J", "A", "S", "O", "N", "D")
+SeasonLabels = c("Winter", "Spring","Summer", "Fall")
+
+#ggplot theme to control formatting parameters for plots with month on the x-axis
+PlotTheme = theme(axis.text=element_text(size=20),    #Text size for axis tick mark labels
+                  axis.title.x=element_blank(),               #Text size and alignment for x-axis label
+                  axis.title.y=element_text(size=24, vjust=0.5,  margin=margin(t=20, r=20, b=20, l=20)),              #Text size and alignment for y-axis label
+                  plot.title=element_text(size=26,face="bold",hjust=0.5, margin=margin(t=20, r=20, b=20, l=20)),      #Text size and alignment for plot title
+                  legend.title=element_text(size=24),                                                                    #Text size of legend category labels
+                  legend.text=element_text(size=22),                                                                   #Text size of legend title
+                  legend.position = "bottom")  
+
+BarPlotTheme = theme(axis.text.x=element_text(size=24),    #Text size for axis tick mark labels
+                     axis.text.y=element_text(size=20),
+                     axis.title.x=element_blank(),               #Text size and alignment for x-axis label
+                     axis.title.y=element_text(size=24, vjust=0.5,  margin=margin(t=20, r=20, b=20, l=20)),              #Text size and alignment for y-axis label
+                     plot.title=element_text(size=26,face="bold",hjust=0.5, margin=margin(t=20, r=20, b=20, l=20)),      #Text size and alignment for plot title
+                     legend.position = "none") 
+
